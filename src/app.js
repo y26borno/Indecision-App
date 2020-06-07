776e8e4d1ea1baa37671c2f3dc27706126c299c3
unknown <yb.26.code@gmail.com>
2020-06-07 03:30:53 -0400

    props for the list of arrays created

--- src/app.js
+++ src/app.js
@@ -1,23 +1,136 @@
+
+
+class IndecisionApp extends React.Component{
+
+
+render(){
+
+ const title = "Indecision"
+ const subtitle = "!!put your in the hands of computer"
+ const options = ['Thing one','Thing two',"sumaya","Rachel"]
+    return (
+
+        <div>
+        
+        <Header title={title} subtitle={subtitle}/>
+        <Action/>
+        <Options options={options}/>
+        <AddOption/>
+        
+        </div>
+    )
+}
+
+
+}
+
+
+
+
+
+
+
 class Header extends React.Component{
 
    render(){
+        
+     console.log(this.props);
 
-
-    return <p>this is from Header</p>;
+    return(
+    
+        <div>
+        <h1>{this.props.title}</h1>
+        <h2>{this.props.subtitle}</h2>
+        </div>
+    ); 
+    
+    
    }
 
 }
 
-const jsx = (
+
+class Action extends React.Component{
+
+render(){
 
 
-    <div>
-    <h1>Title</h1>
+    return (
+
+          <div>
+          
+          <button>What should I do?</button>
+          
+          </div>
+
+    );
+}
+
+}
+
+
+class Options extends React.Component{
+
     
-    <Header/>
-    <Header/>
-    </div>
-);
+
+render(){
+
+     const list = this.props.options;
+      
+        
+     
 
 
-ReactDOM.render(jsx, document.getElementById('app'));
+    return (
+
+        
+        <div>
+        {list.map( (e,key)=> <Option option={e} key={key}/>)}
+        </div>
+    )
+        
+
+
+}
+
+}
+
+class Option extends React.Component{
+
+  render(){
+
+   return (<div>
+   {this.props.option}
+   Option goes here
+   </div>);
+
+  }
+
+}
+
+class AddOption extends React.Component{
+
+    render(){
+
+        return (
+
+
+            <div>
+        Addoption component here
+        </div>
+        );
+  
+  
+  }
+
+}
+
+
+
+
+
+
+
+
+
+ReactDOM.render(<IndecisionApp/>, document.getElementById('app'));
